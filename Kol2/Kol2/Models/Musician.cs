d9596508e61830ca58faf3dae92968c50caa7e6d
Grzegorz Wal <s22042@pjwstk.edu.pl>
2022-06-09 10:38:10 +0200

    2 commit

---
+++ Kol2/Kol2/Models/Musician.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kol2.Models
+{
+    public class Musician
+    {   [Key]
+        public int IdMusician { get; set; }
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+        [Required, MaxLength(100)]
+        public string Nickname { get; set; }
+
+    }
+}
